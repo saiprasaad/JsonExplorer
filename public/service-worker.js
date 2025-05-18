71dc14b9825c66d0dd446652917c5ce569752c4a
Saiprasaad Kalyanaraman <saiprasaad1999@gmail.com>
2025-05-18 17:43:08 -0400

    Minor bug fix

--- public/service-worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  self.clients.claim();
-});
-
-// No fetch handler: this service worker does not cache or intercept requests.
