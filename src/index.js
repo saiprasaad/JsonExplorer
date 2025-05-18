f9ec0cea28c0371261a56b241c1c6e442943e0b3
Saiprasaad Kalyanaraman <saiprasaad1999@gmail.com>
2025-05-18 17:24:52 -0400

    Minor bug fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import LogRocket from 'logrocket';
-import * as serviceWorkerRegistration from './helper/serviceWorkerRegistration';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 LogRocket.init(process.env.REACT_APP_LOGROCKET_ID);
